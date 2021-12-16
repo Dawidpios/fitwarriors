ca2bbc509f38760b583be9cb933a554608c8eac7
Dawidpios <95360547+Dawidpios@users.noreply.github.com>
2021-12-16 16:51:06 +0100

    Update MainPage.js

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -10,7 +10,7 @@ const MainPage = () => {
 
     return ( <>
         <Routes>
-        <Route path="/fitwarriors/" element={<HomePage />}></Route>
+        <Route path="https://dawidpios.github.io/fitwarriors/" element={<HomePage />}></Route>
         <Route path="/calc" element={<Calc/>}></Route>
         <Route path="/food" element={<Food/>}></Route>
        
